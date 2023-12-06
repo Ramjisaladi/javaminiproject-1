95c4e8dc4f02b82bb9eac00fb5ecaa9e3c62aa2b
Ramjisaladi <109979563+Ramjisaladi@users.noreply.github.com>
2023-12-06 18:00:55 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import Moviecard from "./moviecard";
+function App() {
+  return (
+    <div className="App">
+
+      <h1>Movie App</h1>
+      <Moviecard/>
+  
+
+
+
+    </div>
+  );
+}
+
+export default App;
